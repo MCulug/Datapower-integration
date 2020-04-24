bb5c713be4532d9ebb964ef89cd876db7391a730
MCulug <41790613+MCulug@users.noreply.github.com>
2020-04-24 12:50:24 +0300

    Update datapower_audit.py

--- datapower_audit.py
+++ datapower_audit.py
@@ -5,11 +5,11 @@ import filecmp
 import shutil
 import xml.etree.ElementTree as ET
 import base64
-############################################################################################################################################
-#This code requires be trigred with a cronjob or it can be modified to run as a daemon to triger it self. My suggestion is to run every 5 min
-#############################################################################################################################################
-def fetch(path):                                                             #Fetch function takes path to audit file as argument and sends fetch request to server
- request= """<?xml version="1.0" encoding="UTF-8"?>                                  
+##############################################################################################################################################
+#This code requires be trigred with a cronjob or it can be modified to run as a daemon to triger it self. My suggestion is to run every 5 min#
+##############################################################################################################################################
+def fetch(path):                                                            #Fetch function takes path to audit file as argument and sends fetch request to server
+ request= """<?xml version="1.0" encoding="UTF-8"?>
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
 <soapenv:Body>
 <dp:request domain="default" xmlns:dp="http://www.datapower.com/schemas/management">
@@ -17,49 +17,49 @@ def fetch(path):                                                             #Fe
 </dp:request>
 </soapenv:Body>
 </soapenv:Envelope>""" %path
- host="https://yourDatapowerHost:5550"                                        #5550 is the default port of xml management interface in IBM datapower server
- AUTH = 'admin', 'XXXXXXXXX'
+ host="https://your_target_host_address:5550"
+ AUTH = 'admin', 'XXXXXXX'                                                  #5550 is the default port of xml management interface in IBM datapower server which must be enabled before execution
  headers = {'Content-Type':'text/xml;charset=UTF-8'}
  z = requests.post(host, data=request, verify=False, headers=headers, auth=(AUTH))
  root = ET.fromstring(z.content)
  for child in root.iter('*'):
-  if(str(child.tag) == "{http://www.datapower.com/schemas/management}file"):  #The server response is base64 encoded. We will decode and rename it 
+  if(str(child.tag) == "{http://www.datapower.com/schemas/management}file"): #The server response is base64 encoded. We will decode and rename it
    base64_message = child.text
    base64_bytes = base64_message.encode('ascii')
    message_bytes = base64.b64decode(base64_bytes)
    message = message_bytes.decode('ascii')
-   file = open("audit_new.txt", "w+")
+   file = open("audit_new", "w+")
    file.write(message)
    file.close()
 
 if os.path.isfile('switch/audit-log'):                                        #Here is the switch mechanism. These two if statements will switch audit-log and audit-log.1 file
  print ("Working with audit-log file")
  fetch("audit:///audit-log")
- if filecmp.cmp('audit_new.txt', 'switch/audit-log') == False:
-  print('files sizes are different. New log file will be overriden')          #/switch and /audit directories must be created before running code. 
+ if filecmp.cmp('audit_new', 'switch/audit-log') == False:
+  print('files sizes are different. New audit-log file will be overriden')    #/switch and /audit directories must be created before running code. 
   os.remove('switch/audit-log')
-  shutil.move('audit_new.txt', 'switch/audit_new.txt')
-  os.rename('switch/audit_new.txt', 'switch/audit-log')
+  shutil.move('audit_new', 'switch/audit_new')
+  os.rename('switch/audit_new', 'switch/audit-log')
  else:
   time = str(time.strftime("%d %B %Y %H:%M:%S"))
   print('files match. Move file to permanant audit directory and switching to other log file audit-log.1')
   shutil.move('switch/audit-log', 'Audit/audit-log')
-  os.rename('Audit/audit-log', 'Audit/'+time+'.txt')
-  file = open("audit-log.1","w")
+  os.rename('Audit/audit-log', 'Audit/'+time)
+  file = open("switch/audit-log.1","w")
   file.close()
 
 if os.path.isfile('switch/audit-log.1'):
  print ("Working with audit-log.1 file")
  fetch("audit:///audit-log.1")
- if filecmp.cmp('audit_new.txt', 'switch/audit-log.1') == False:
-  print('files sizes are different. New log file will be overriden')
+ if filecmp.cmp('audit_new', 'switch/audit-log.1') == False:
+  print('files sizes are different. New audit-log.1 file will be overriden')
   os.remove('switch/audit-log.1')
-  shutil.move('audit_new.txt', 'switch/audit_new.txt')
-  os.rename('switch/audit_new.txt', 'switch/audit-log.1')
+  shutil.move('audit_new', 'switch/audit_new')
+  os.rename('switch/audit_new', 'switch/audit-log.1')
  else:
   time = str(time.strftime("%d %B %Y %H:%M:%S"))
   print('files match. Move file to permanant audit directory and switching to other log file audit-log')
   shutil.move('switch/audit-log.1', 'Audit/audit-log.1')
-  os.rename('Audit/audit-log.1', 'Audit/'+time+'.txt')
-  file = open("audit-log","w")
+  os.rename('Audit/audit-log.1', 'Audit/'+time)
+  file = open("switch/audit-log","w")
   file.close()
